0fc14be0fe7d5b49c244a5b341673837f96db712
Vinay <vinu_009@hotmail.com>
2020-11-21 16:59:14 +0000

    Setup production build

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+//function routes the GET requests to the path, that is specified with the callback function
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server is up and running!');
+});
